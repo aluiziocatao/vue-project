9c203fcf95b1e70acc4f559e095577e7c3efde79
Aluízio Catão <aluizio2210@gmail.com>
2023-07-27 17:54:46 -0500

    Utilizando JSON server e implementando Vue Router

---
+++ src/components/Navbar.vue
@@ -0,0 +1,21 @@
+<template>
+	<div>
+		<nav>
+			<router-link to="/">
+				<img src="" alt="" id="logo">
+			</router-link>
+			<router-link to="/">Home</router-link> |
+			<router-link to="/pedidos">Pedidos</router-link>
+		</nav>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "Navbar"
+}
+</script>
+
+<style scoped>
+
+</style>
